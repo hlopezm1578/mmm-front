e1a5cb1cde63395b0660aa97f96f69bd7706ddca
HANS EDUARDO LOPEZ MEDINA <hlopez_m@hotmail.com>
2022-06-21 18:35:39 -0400

    mm3

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { retry } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
@@ -10,38 +9,69 @@ import { BackendService } from 'src/app/services/backend.service';
 export class HomeComponent implements OnInit {
 
   assets:any[]=[];
+  totalProfit: number = 0;
+  totalOperations:number = 0;
+  winPos : number = 0;
+  losePos:number = 0;
 
   constructor(private service:BackendService) { }
 
   ngOnInit(): void {
     this.service.getAssets().subscribe((resp:any)=>{
       if(resp.ok){
-        this.assets = resp.assets
-        this.assets.forEach(asset => {
-          var wins = asset.positions.filter((position:any)=>{
-            if(position.result>0){
-              return true;
-            }else{
-              return false;
-            }
-          }).length;
-          var loses = asset.positions.filter((position:any)=>{
-            if(position.result<0){
-              return true;
-            }else{
-              return false;
-            }
-          }).length;
-          asset.total = wins+loses;
-          asset.loses = loses;
-          asset.wins = wins;
-          var wr = asset.total>0 ? asset.wins/asset.total : 0;
-          asset.wr = wr;
-        });
+        var processAssets = this.processData(resp.assets);
+        this.assets = processAssets;
+        this.totalProfit = this.getTotalProfit(processAssets);
         console.log(resp);
       }
      
     })
+  }
+
+  processData(assets:any){
+    assets.forEach((asset:any) => {
+      var wins = asset.positions.filter((position:any)=>{
+        if(position.result>0){
+          return true;
+        }else{
+          return false;
+        }
+      }).length;
+      var loses = asset.positions.filter((position:any)=>{
+        if(position.result<0){
+          return true;
+        }else{
+          return false;
+        }
+      }).length;
+      asset.total = wins+loses;
+      asset.loses = loses;
+      asset.wins = wins;
+      asset.pnWins = wins*4;
+      asset.pnLoses = loses*2;
+      asset.pnTotal = asset.pnWins - asset.pnLoses;
+      asset.profit = asset.pnTotal/100;
+      var wr = asset.total>0 ? asset.wins/asset.total : 0;
+      asset.wr = wr;
+    });
+    return assets;
+  }
+
+  getTotalProfit(assets:any){
+    var sum=0;
+    var wins = 0;
+    var loses = 0;
+    assets.forEach((asset:any) => {
+      sum += parseFloat(asset.profit);
+      wins += asset.wins;
+      loses += asset.loses;
+    })
+
+    this.winPos = wins;
+    this.losePos = loses;
+    this.totalOperations = wins+loses;
+
+    return sum;
   }
 
 }
